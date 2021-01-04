4ffcc6746ee05bf01764724af72dbd05aed361fd
Mauro Lepore <maurolepore@gmail.com>
2021-01-04 13:44:26 -0600

    Polish test

--- tests/testthat/test-checkout.R
+++ tests/testthat/test-checkout.R
@@ -1,22 +1,15 @@
 library(gert)
 
-test_that("with the current working directory does nothing", {
+test_that("with the working repo leaves the repo at the HEAD", {
+  stopifnot(user_is_configured())
   repo <- withr::local_tempdir()
   withr::local_dir(repo)
   git_init(repo)
-
-  # Set a user if no default
-  if (!user_is_configured()) {
-    git_config_set("user.name", "Jerry")
-    git_config_set("user.email", "jerry@gmail.com")
-  }
-
-
 
   file.create("a")
   gert::git_add("a")
   id <- gert::git_commit("New file")
 
   checkout(repo)
-  expect_equal(gert::git_commit_id(repo = repo), id)
+  expect_equal(gert::git_commit_id(ref = "HEAD", repo = repo), id)
 })
