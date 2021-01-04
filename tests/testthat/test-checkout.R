587910c677327b3e594d3188a15dbaf2df4d2481
Mauro Lepore <maurolepore@gmail.com>
2021-01-04 13:46:59 -0600

    Don't namespace

--- tests/testthat/test-checkout.R
+++ tests/testthat/test-checkout.R
@@ -1,15 +1,16 @@
 library(gert)
+library(withr)
 
 test_that("with the working repo leaves the repo at the HEAD", {
   stopifnot(user_is_configured())
-  repo <- withr::local_tempdir()
-  withr::local_dir(repo)
+  repo <- local_tempdir()
+  local_dir(repo)
   git_init(repo)
 
   file.create("a")
-  gert::git_add("a")
-  id <- gert::git_commit("New file")
+  git_add("a")
+  id <- git_commit("New file")
 
   checkout(repo)
-  expect_equal(gert::git_commit_id(ref = "HEAD", repo = repo), id)
+  expect_equal(git_commit_id(ref = "HEAD", repo = repo), id)
 })
