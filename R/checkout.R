445bdd57b7f1d80610941f29451c2b45708441f3
Mauro Lepore <maurolepore@gmail.com>
2021-01-04 14:00:33 -0600

    Remove examples for now
    Wait until better examples inspired by tests.

--- R/checkout.R
+++ R/checkout.R
@@ -6,24 +6,6 @@
 #'
 #' @return Called for its side effect. Returns `repos` invisibly.
 #' @export
-#'
-#' @examples
-#' library(gert)
-#' library(withr)
-#'
-#' # Setup
-#' stopifnot(user_is_configured())
-#' repo <- local_tempdir()
-#' local_dir(repo)
-#' git_init(repo)
-#' file.create("a")
-#' git_add("a")
-#' git_commit("New file")
-#'
-#' # The commit sha is the same before and after `checkout()`
-#' gert::git_commit_id("HEAD", repo = repo)
-#' checkout(repo)
-#' gert::git_commit_id("HEAD", repo = repo)
 checkout <- function(repos) {
   invisible(repos)
 }
