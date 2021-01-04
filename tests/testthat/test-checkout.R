af499e26b1b9435d4d694ba4b4682d1ec9293997
Mauro Lepore <maurolepore@gmail.com>
2021-01-04 14:06:26 -0600

    Test from outside the working directory

--- tests/testthat/test-checkout.R
+++ tests/testthat/test-checkout.R
@@ -1,7 +1,7 @@
 library(gert)
 library(withr)
 
-test_that("with the working repo leaves the repo at the HEAD", {
+test_that("from the working directory, checkouts the HEAD", {
   repo <- local_tempdir()
   local_dir(repo)
   git_init(repo)
@@ -10,6 +10,18 @@ test_that("with the working repo leaves the repo at the HEAD", {
   git_add("a")
 
   id <- git_commit("New file")
+  checkout(repo)
+  expect_equal(git_commit_id(ref = "HEAD", repo = repo), id)
+})
+
+test_that("from outside the working directory, checkouts the main branch", {
+  repo <- local_tempdir()
+  git_init(repo)
+
+  file.create(file.path(repo, "a"))
+  git_add("a", repo = repo)
+
+  id <- git_commit("New file", repo = repo)
   checkout(repo)
   expect_equal(git_commit_id(ref = "HEAD", repo = repo), id)
 })
